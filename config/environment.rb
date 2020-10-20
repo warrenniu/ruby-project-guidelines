2e8754905300b1a2bf6995dc520a6d15c207f066
warrenniu <warrenniu@gmail.com>
2020-10-20 18:20:14 -0400

    Environment_update

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 require 'bundler/setup'
 Bundler.require
 
+ActiveRecord::Base.logger = nil
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'app'
